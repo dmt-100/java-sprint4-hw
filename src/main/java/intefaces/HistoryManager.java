beb5b463c8370d9751232cf399efbc55d2768353
dmt-100 <dmt-100@yandex.ru>
2022-12-24 05:02:36 +0300

    Первая проверка

---
+++ src/main/java/intefaces/HistoryManager.java
@@ -0,0 +1,16 @@
+package main.java.intefaces;
+
+import main.java.tasks.Task;
+import java.util.HashMap;
+import java.util.List;
+
+public interface HistoryManager {
+
+    void add(Task task); // функция этого метода перешла в класс inMemoryTaskManager (см. со 131 строки)
+    // Managers.getDefaultHistory().getHistory().add(tasks.get(idInput)); // ТЗ-4 добавляем в лист просмотренную
+
+    List<Task> getHistory();
+
+    void checkList();
+
+}
